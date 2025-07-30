badde71bbe92a6ff2b221444b1d7f9f7948d2a61
Sebastián Mendoza Farías <snmendoz@uc.cl>
2025-07-30 16:47:05 -0400

    update functions

--- code/metabolix_flux_analysis.m
+++ code/metabolix_flux_analysis.m
@@ -71,14 +71,10 @@ end
 % check the new lower bounds
 medianew = [model.rxns(posEX) model.rxnNames(posEX) num2cell(model.lb(posEX))];
 
+%% GENERATE MUTANT STRAINS AND OBTAIN FLUX DISTRIBUTIONS
+
 % perfom Flux Balance Analysis for wild-type strain
 fba_wt_aux = optimizeCbModel(model);
-
-% compute scaled reduced costs for wild-type strain
-pos_scaled_reduced_cost = find(abs(fba_wt_aux.w.*fba_wt_aux.x)>1e-6);
-scale_reduced_costs = fba_wt_aux.w.*fba_wt_aux.x;
-scale_reduced_cost_rxns = model.rxns(pos_scaled_reduced_cost);
-scale_reduced_cost_values = scale_reduced_costs(pos_scaled_reduced_cost);pos_scaled_reduced_cost = find(abs(fba_wt_aux.w.*fba_wt_aux.x)>1e-6);
 
 % perfom parsimonious Flux Balance Analysis for wild-type strain
 [~, ~, ~, fba_wildtype] = pFBA_own(model, 'skipclass', 1,'tol',10^-7);
@@ -89,7 +85,6 @@ for i = 1:length(model.grRules)
         model.grRules{i}= ''; 
     end
 end
-
 
 % find the gene associated to gene EF3089 (glutathione synthetase)
 pos_gsh = find(contains(model.grRules,'EF3089'));%GSH 3089
@@ -129,7 +124,7 @@ save([baseFolder filesep 'results' filesep 'models' filesep 'gsh_arc_mutant'],'g
 % perform parsimonious Flux Balance Analysis for the double mutant strain ∆gsh∆arc
 [~, ~, ~, fba_DKO_mutante] = pFBA_own(gsh_arc_mutant, 'skipclass', 1,'tol',10^-7);
 
-%visualize fluxes for different strains
+%% VISUALIZE FLUXES FOR DIFFERENT STRAINS
 colors = [
     0.1059  0.6196  0.4667; % Color 1
     0.8510  0.3725  0.0078; % Color 2
@@ -168,134 +163,8 @@ title('Specific growth rate for different strains');
 % save figure
 saveas(gcf, [baseFolder filesep 'results' filesep 'plots' filesep 'specific_growth_rate_comparison.pdf']);    
 
-%ATPrxn 
-atpIndex = find(strcmp(model.mets, 'atp_c'));  
-fluxes = fba_wildtype;
-atpStoichiometry = model.S(atpIndex, :);
-atpConsumingReactions = {};
-atpProducingReactions = {};
-for i = 1:length(model.rxns)
-    if atpStoichiometry(i) < 0 && fluxes(i) > 0
-        atpConsumingReactions{end+1} = model.rxns{i};
-    elseif atpStoichiometry(i) > 0 && fluxes(i) < 0
-        atpConsumingReactions{end+1} = model.rxns{i};
-    elseif atpStoichiometry(i) > 0 && fluxes(i) > 0
-        atpProducingReactions{end+1} = model.rxns{i};
-    elseif atpStoichiometry(i) < 0 && fluxes(i) < 0
-        atpProducingReactions{end+1} = model.rxns{i};
-    end
-end
-disp('ATP Consuming Reactions:');
-disp(atpConsumingReactions);
-disp('ATP Producing Reactions:');
-disp(atpProducingReactions);
-%ADP rxn
-adpIndex = find(strcmp(model.mets, 'adp_c'));  
-fluxes = fba_wildtype;
-adpStoichiometry = model.S(adpIndex, :);
-adpConsumingReactions = {};
-adpProducingReactions = {};
-for i = 1:length(model.rxns)
-    if adpStoichiometry(i) < 0 && fluxes(i) > 0
-        adpConsumingReactions{end+1} = model.rxns{i};
-    elseif adpStoichiometry(i) > 0 && fluxes(i) < 0
-        adpConsumingReactions{end+1} = model.rxns{i};
-    elseif adpStoichiometry(i) > 0 && fluxes(i) > 0
-        adpProducingReactions{end+1} = model.rxns{i};
-    elseif adpStoichiometry(i) < 0 && fluxes(i) < 0
-        adpProducingReactions{end+1} = model.rxns{i};
-    end
-end
-disp('ADP Consuming Reactions:');
-disp(adpConsumingReactions);
-disp('ADP Producing Reactions:');
-disp(adpProducingReactions);
-%NADP rxn
-nadpIndex = find(strcmp(model.mets, 'nadp_c'));  
-fluxes = fba_wildtype;
-nadpStoichiometry = model.S(nadpIndex, :);
-nadpConsumingReactions = {};
-nadpProducingReactions = {};
-for i = 1:length(model.rxns)
-    if nadpStoichiometry(i) < 0 && fluxes(i) > 0
-        nadpConsumingReactions{end+1} = model.rxns{i};
-    elseif nadpStoichiometry(i) > 0 && fluxes(i) < 0
-        nadpConsumingReactions{end+1} = model.rxns{i};
-    elseif nadpStoichiometry(i) > 0 && fluxes(i) > 0
-        nadpProducingReactions{end+1} = model.rxns{i};
-    elseif nadpStoichiometry(i) < 0 && fluxes(i) < 0
-        nadpProducingReactions{end+1} = model.rxns{i};
-    end
-end
-disp('NADP Consuming Reactions:');
-disp(nadpConsumingReactions);
-disp('NADP Producing Reactions:');
-disp(nadpProducingReactions);
-%NAD rxn
-nadIndex = find(strcmp(model.mets, 'nad_c'));  
-fluxes = fba_wildtype;
-nadStoichiometry = model.S(nadIndex, :);
-nadConsumingReactions = {};
-nadProducingReactions = {};
-for i = 1:length(model.rxns)
-    if nadStoichiometry(i) < 0 && fluxes(i) > 0
-        nadConsumingReactions{end+1} = model.rxns{i};
-    elseif nadStoichiometry(i) > 0 && fluxes(i) < 0
-        nadConsumingReactions{end+1} = model.rxns{i};
-    elseif nadStoichiometry(i) > 0 && fluxes(i) > 0
-        nadProducingReactions{end+1} = model.rxns{i};
-    elseif nadStoichiometry(i) < 0 && fluxes(i) < 0
-        nadProducingReactions{end+1} = model.rxns{i};
-    end
-end
-disp('NAD Consuming Reactions:');
-disp(nadConsumingReactions);
-disp('NAD Producing Reactions:');
-disp(nadProducingReactions);
-%NADH rxn
-nadhIndex = find(strcmp(model.mets, 'nadh_c'));  
-fluxes = fba_wildtype;
-nadhStoichiometry = model.S(nadhIndex, :);
-nadhConsumingReactions = {};
-nadhProducingReactions = {};
-for i = 1:length(model.rxns)
-    if nadhStoichiometry(i) < 0 && fluxes(i) > 0
-        nadhConsumingReactions{end+1} = model.rxns{i};
-    elseif nadhStoichiometry(i) > 0 && fluxes(i) < 0
-        nadhConsumingReactions{end+1} = model.rxns{i};
-    elseif nadhStoichiometry(i) > 0 && fluxes(i) > 0
-        nadhProducingReactions{end+1} = model.rxns{i};
-    elseif nadhStoichiometry(i) < 0 && fluxes(i) < 0
-        nadhProducingReactions{end+1} = model.rxns{i};
-    end
-end
-disp('NADH Consuming Reactions:');
-disp(nadhConsumingReactions);
-disp('NADH Producing Reactions:');
-disp(nadhProducingReactions);
-%NADPH rxn
-nadphIndex = find(strcmp(model.mets, 'nadph_c'));  
-fluxes = fba_wildtype;
-nadphStoichiometry = model.S(nadphIndex, :);
-nadphConsumingReactions = {};
-nadphProducingReactions = {};
-for i = 1:length(model.rxns)
-    if nadphStoichiometry(i) < 0 && fluxes(i) > 0
-        nadphConsumingReactions{end+1} = model.rxns{i};
-    elseif nadphStoichiometry(i) > 0 && fluxes(i) < 0
-        nadphConsumingReactions{end+1} = model.rxns{i};
-    elseif nadphStoichiometry(i) > 0 && fluxes(i) > 0
-        nadphProducingReactions{end+1} = model.rxns{i};
-    elseif nadphStoichiometry(i) < 0 && fluxes(i) < 0
-        nadphProducingReactions{end+1} = model.rxns{i};
-    end
-end
-disp('NADPH Consuming Reactions:');
-disp(nadphConsumingReactions);
-disp('NADPH Producing Reactions:');
-disp(nadphProducingReactions);
 
-
+%% EXPORT FLUX DISTRIBUTIONS
 
 % Wild-type
 rxns_wt = model.rxns;
@@ -323,74 +192,3 @@ writetable(wt_table, [baseFolder filesep 'results' filesep 'tables' filesep file
 writetable(gsh_table, [baseFolder filesep 'results' filesep 'tables' filesep filename], 'Sheet', 'Mutant_GSH');
 writetable(arg_table, [baseFolder filesep 'results' filesep 'tables' filesep filename], 'Sheet', 'Mutant_Arginine');
 writetable(dko_table, [baseFolder filesep 'results' filesep 'tables' filesep filename], 'Sheet', 'Double_Knockout');
-
-%%%%%
-
-fba_wt_gsh = optimizeCbModel(gsh_mutant);
-fba_wt_arg = optimizeCbModel(arc_mutant);
-fba_wt_dko = optimizeCbModel(gsh_arc_mutant);
-
-%compute reduced costs and scaled reduced costs for the strains
-pos_scaled_reduced_cost1 = find(abs(fba_wt_gsh.w.*fba_wt_gsh.x)>1e-6);
-scale_reduced_costs1 = fba_wt_gsh.w.*fba_wt_gsh.x;
-scale_reduced_cost_rxns1 = gsh_mutant.rxns(pos_scaled_reduced_cost1);
-scale_reduced_cost_values1 = scale_reduced_costs1(pos_scaled_reduced_cost1);
-
-pos_scaled_reduced_cost2 = find(abs(fba_wt_arg.w.*fba_wt_arg.x)>1e-6);
-scale_reduced_costs2 = fba_wt_arg.w.*fba_wt_arg.x;
-scale_reduced_cost_rxns2 = arc_mutant.rxns(pos_scaled_reduced_cost2);
-scale_reduced_cost_values2 = scale_reduced_costs2(pos_scaled_reduced_cost2);
-
-pos_scaled_reduced_cost3 = find(abs(fba_wt_dko.w.*fba_wt_dko.x)>1e-6);
-scale_reduced_costs3 = fba_wt_dko.w.*fba_wt_dko.x;
-scale_reduced_cost_rxns3 = gsh_arc_mutant.rxns(pos_scaled_reduced_cost3);
-scale_reduced_cost_values3 = scale_reduced_costs3(pos_scaled_reduced_cost3);
-disp('')
-
-%polyamines pathway
-% List of metabolites
-mets = {'orn_L_c', 'ptrc_c', 'spmd_c', 'arg_L', 'agma_c', 'cbp_c', 'amet_c', 'p15da_c'};
-
-% Initialize a cell array to hold reactions for each metabolite
-reactions = cell(length(mets), 2);
-
-% Loop through each metabolite
-for i = 1:length(mets)
-    % Find indexes of reactions that involve the current metabolite
-    rxnIndexes = find(contains(model.mets, mets{i}));
-    
-    % Get the IDs of these reactions
-    reactions{i, 1} = mets{i};
-    reactions{i, 2} = model.rxns(rxnIndexes);
-end
-
-% Display reactions for each metabolite
-reactions
-
-% Extract reaction IDs and names
-rxnIDs = model.rxns;
-rxnNames = model.rxnNames;
-
-% Extract reactions and their corresponding gene reaction rules
-reactions = model.rxns;
-geneReactionRules = model.grRules;
-
-% Create a table with the reactions and their gene reaction rules
-reactionTab = table(reactions, geneReactionRules, 'VariableNames', {'Reactions', 'GeneReactionRules'});
-
-% Display the table
-disp(reactionTab);
-
-% Crear una tabla con las reacciones y sus límites
-reactionIDs = model.rxns;
-lowerBounds = model.lb;
-upperBounds = model.ub;
-
-% Create table with reactions and bounds
-reactionlimit = table(reactionIDs, lowerBounds, upperBounds, ...
-                      'VariableNames', {'ReactionID', 'LowerBound', 'UpperBound'});
-
-% save table as excel file
-writetable(reactionlimit, [baseFolder filesep 'results' filesep 'tables' filesep 'reactions_bounds.xlsx']);
-
-disp('')
